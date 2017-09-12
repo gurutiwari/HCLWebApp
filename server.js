b578f8d1f57c21e82b95eb05ee1e4caf1e7d60f4
gurutiwari <tiwarig@hcl.com>
2017-09-12 18:33:03 +0530

    ok

--- server.js
+++ server.js
@@ -5,7 +5,25 @@ const moment = require('moment');
 const path = require('path');
 const iotHubClient = require('./IoTHub/iot-hub.js');
 
+const cors = require('cors');
+  
+//app.user(bodyParser.json());
+// after the code that uses bodyParser and other cool stuff
+var originsWhitelist = [
+  'http://localhost:4200',      //this is my front-end url for development
+   'http://www.myproductionurl.com'
+];
+var corsOptions = {
+  origin: function(origin, callback){
+        var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
+        callback(null, isWhitelisted);
+  },
+  credentials:true
+}
 const app = express();
+
+//here is the magic
+app.use(cors(corsOptions));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(function (req, res/*, next*/) {
